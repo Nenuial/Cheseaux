9821635493c3cfbb1c6bba8a0512395df223e288
Pascal Burkhard <pascal.burkhard@gmail.com>
2023-08-27 08:53:01 +0200

    Fix path

--- R/fetch.R
+++ R/fetch.R
@@ -5,7 +5,7 @@ drive_auth(path = ".secrets/equievents-368612-5bb2b3c9038e.json")
 drive_ls("Concours/Cheseaux") |> 
   purrr::pwalk(~drive_download(..2, path = paste0("data/", ..1), overwrite = T))
 
-# fs::dir_create("data/champ")
-# 
-# drive_ls("Concours/MaleyChamp") |> 
-#   purrr::pwalk(~drive_download(..2, path = paste0("data/champ/", ..1), overwrite = T))
+fs::dir_create("data/champ")
+
+drive_ls("Concours/MaleyChamp") |>
+  purrr::pwalk(~drive_download(..2, path = paste0("data/champ/", ..1), overwrite = T))

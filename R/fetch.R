ddf9572a364200afd0dcfffc2e2c2c49bd9e3c4f
Pascal Burkhard <pascal.burkhard@gmail.com>
2026-07-03 22:39:42 +0200

    Avoid Champ downloads

--- R/fetch.R
+++ R/fetch.R
@@ -2,10 +2,12 @@ library(googledrive)
 
 drive_auth(path = ".secrets/equievents-368612-5bb2b3c9038e.json")
 
-drive_ls("Concours/Cheseaux") |> 
-  purrr::pwalk(~drive_download(..2, path = paste0("data/", ..1), overwrite = T))
+drive_ls("Concours/Cheseaux") |>
+  purrr::pwalk(
+    ~ drive_download(..2, path = paste0("data/", ..1), overwrite = T)
+  )
 
-fs::dir_create("data/champ")
+# fs::dir_create("data/champ")
 
-drive_ls("Concours/CheseauxChamp") |>
-  purrr::pwalk(~drive_download(..2, path = paste0("data/champ/", ..1), overwrite = T))
+# drive_ls("Concours/CheseauxChamp") |>
+#   purrr::pwalk(~drive_download(..2, path = paste0("data/champ/", ..1), overwrite = T))
